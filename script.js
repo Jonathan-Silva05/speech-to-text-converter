8dc22010b42c55faab452169b1bcab6bf47d441a
Jonathan-Silva05 <133710242+Jonathan-Silva05@users.noreply.github.com>
2024-04-06 12:44:33 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,56 +1,7 @@
-/*// cria as constantes que referenciam os botões e a área de texto onde iremos transcrever o áudio
-const texto = document.querySelector(".texto");
-const btnStart = document.querySelector("#btnStart");
-const btnStop = document.querySelector("#btnStop");
-
-// configurar o webkit de reconhecimento de fala
-window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-const recognition = new SpeechRecognition();
-recognition.interimResults = true; // Se definido como false o resultado será apresentado após a fala
-recognition.lang = "pt-BR";
-
-// criando uma nova tag p
-let p = document.createElement("p");
-
-// Inicializa a transcrição com o parágrafo recém-criado
-texto.appendChild(p);
-
-// Função para lidar com os resultados do reconhecimento de fala
-recognition.addEventListener('result', e => {
-  // Converte o resultado do reconhecimento de fala em texto
-  const transcript = Array.from(e.results)
-    .map(result => result[0])
-    .map(result => result.transcript)
-    .join('');
-
-  // Atualiza o texto do parágrafo com a transcrição
-  p.textContent = transcript;
-
-  // Se o resultado for final, cria um novo parágrafo para a próxima transcrição
-  if (e.results[0].isFinal) {
-    p = document.createElement("p");
-    texto.appendChild(p);
-  }
-});
-
-// Inicia o reconhecimento de fala quando o botão 'Iniciar' é clicado
-btnStart.addEventListener('click', () => {
-  recognition.start();
-});
-
-// Para o reconhecimento de fala quando o botão 'Parar' é clicado
-btnStop.addEventListener('click', () => {
-  recognition.stop();
-});
-
-// Reinicia o reconhecimento de fala automaticamente quando termina
-recognition.addEventListener('end', recognition.start);*/
-
-
 document.addEventListener("DOMContentLoaded", function() {
   var recognition = new webkitSpeechRecognition() || new SpeechRecognition();
   recognition.lang = 'pt-BR'; // Definir o idioma para português do Brasil
-  recognition.continuous = false; // Não continuar ouvindo após a primeira transcrição
+  recognition.continuous = true; // Continuar ouvindo após a primeira transcrição
 
   // Botões e elementos do DOM
   var btnStart = document.getElementById("btnStart");
